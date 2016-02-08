f4abf918b5974bde2a87a3ab11cd2ea01b7a3cfb
Guy Taylor <thebigguy.co.uk@gmail.com>
2016-02-08 20:24:49 +0000

    Update README and setup.py tags

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pypcappy
-Pure Python3 PcapNg reader
+PyPcapPy
+========
+**A Pure Python3 PcapNg reader**
+
+Use
+===
+```python
+with PcapNgFile('file_name.pcapng.gz') as pcap_file:
+  for packet in pcap_file.packets:
+    print(packet)
+```
+
+Why not pypcap or pypcapfile?
+=============================
+* Pure python
+** Will work on CPython, PyPy, Jpython or whatever
+* Python 3
+** No legacy packet data as strings
+* PcapNG
+** Only support for the new *next gen* format
+* Gzip
+** Build in support for both *.pcapng and *.pcapng.gz files
--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+import os
 
 setup(
     name = 'pypcappy',
     packages = ['pypcappy', 'pypcappy.blocks'], 
     version = '0.0.1',
     description = 'Pure Python3 PcapNg reader',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     author = 'Guy Taylor',
     author_email = 'thebigguy.co.uk@gmail.com',
     url = 'https://github.com/TheBiggerGuy/pypcappy',
     keywords = ['pcap'], 
-    classifiers = [],  
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],  
 )
